aade6db3319b99e92a7419ddb7095b2b9510b386
TanuShree2154 <tanushreesoni6644@gmail.com>
2026-06-04 21:42:38 -0700

    added menu

---
+++ main.py
@@ -0,0 +1,12 @@
+print("""choose option to perfrom operation:
+      1. addition 
+      2. substraction 
+      3. multiply 
+      4. division 
+      5. power
+      6. modulo 
+      7. floor division
+      8. exit
+      """)
+ch = int(input("enter your choice:"))
+
